74111afaede5ff65c17847ce25c8fdbf9f172ab0
Kévin Roussel <9155865+KevinRoussel@users.noreply.github.com>
2024-01-12 14:01:58 +0100

    CharacterMove + AnimatorBinding implementation

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -12,27 +12,65 @@ using UnityEngine.SceneManagement;
 public class PlayerMove : MonoBehaviour
 {
     [SerializeField] InputActionReference _move;
-    [SerializeField] float _speed;
+    [SerializeField] Rigidbody _rb;
+    [SerializeField, Range(0, 100)]
+    float _speed;
 
-    // Event pour les dev
+
     public event Action OnStartMove;
-    public event Action<int> OnHealthUpdate;
+    public event Action OnStopMove;
 
-    // Event pour les GD
-    [SerializeField] UnityEvent _onEvent;
-    [SerializeField] UnityEvent _onEventPost;
 
-    public Vector2 JoystickDirection { get; private set; }
     Coroutine MovementRoutine { get; set; }
+
+    private void Reset()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _speed = 10f;
+    }
+
+    private void OnValidate()
+    {
+        if(_speed <= 0)
+        {
+            Debug.LogWarning("Attention");
+            _speed = 10;
+        }
+    }
+
+
 
     private void Start()
     {
-        
+        _move.action.started += StartMove;
+        _move.action.canceled += StopMove;
+    }
+    private void StartMove(InputAction.CallbackContext obj)
+    {
+        MovementRoutine = StartCoroutine(Move());
+        IEnumerator Move()
+        {
+            OnStartMove?.Invoke();
+            while (true)
+            {
+                var direction = obj.ReadValue<Vector2>();
+                var v3 = new Vector3(direction.x, 0, direction.y);
+                _rb.velocity = v3 * _speed;
+
+                yield return null;
+            }
+        }
     }
 
-    private void OnDestroy()
+    private void StopMove(InputAction.CallbackContext obj)
     {
-        
+        OnStopMove?.Invoke();
+        _rb.velocity = Vector3.zero;
+
+        StopCoroutine(MovementRoutine);
+        MovementRoutine = null;
     }
+
+
 
 }
